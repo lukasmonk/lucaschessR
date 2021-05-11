1c93654f868a539187fb703dba24a88a737688b5
lukasmonk <lukasmonk@gmail.com>
2021-05-11 17:17:25 +0200

    Fixed linux64/2

--- bin/_fastercode/linux64.sh
+++ bin/_fastercode/linux64.sh
@@ -30,21 +30,21 @@ echo ">> Arasan" && chmod 755 ../../OS/linux/Engines/arasan/Arasan-22.2
 echo ">> Asymptote" && chmod 755 ../../OS/linux/Engines/asymptote/Asymptote-0.8
 echo ">> Beef" && chmod 755 ../../OS/linux/Engines/beef/Beef-0.36
 echo ">> Cassandre" && chmod 755 ../../OS/linux/Engines/cassandre/Cassandre-0.24
-echo ">> CeeChess" && chmod 755 ../../OS/linux/Engines/ceeChess/CeeChess-1.3.2
+echo ">> CeeChess" && chmod 755 ../../OS/linux/Engines/ceechess/CeeChess-1.3.2
 echo ">> Cheng" && chmod 755 ../../OS/linux/Engines/cheng/Cheng-4.40
 echo ">> Chessika" && chmod 755 ../../OS/linux/Engines/chessika/Chessika-2.21
 echo ">> Cinnamon" && chmod 755 ../../OS/linux/Engines/cinnamon/Cinnamon-1.2b
 echo ">> Clarabit" && chmod 755 ../../OS/linux/Engines/clarabit/Clarabit-1.00
 echo ">> Counter" && chmod 755 ../../OS/linux/Engines/counter/Counter-3.7
 echo ">> Critter" && chmod 755 ../../OS/linux/Engines/critter/Critter-1.6a
-echo ">> CT800" && chmod 755 ../../OS/linux/Engines/cT800/CT800-1.42
+echo ">> CT800" && chmod 755 ../../OS/linux/Engines/ct800/CT800-1.42
 echo ">> Daydreamer" && chmod 755 ../../OS/linux/Engines/daydreamer/Daydreamer-1.75
 echo ">> Delocto" && chmod 755 ../../OS/linux/Engines/delocto/Delocto-0.61n
 echo ">> Discocheck" && chmod 755 ../../OS/linux/Engines/discocheck/Discocheck-5.2.1
 echo ">> Dragontooth" && chmod 755 ../../OS/linux/Engines/dragontooth/Dragontooth-0.2
 echo ">> Drofa" && chmod 755 ../../OS/linux/Engines/drofa/Drofa-2.2.0
 echo ">> Ethereal" && chmod 755 ../../OS/linux/Engines/ethereal/Ethereal-12.75
-echo ">> FracTal" && chmod 755 ../../OS/linux/Engines/fracTal/FracTal-1.0
+echo ">> FracTal" && chmod 755 ../../OS/linux/Engines/fractal/FracTal-1.0
 echo ">> Fruit" && chmod 755 ../../OS/linux/Engines/fruit/Fruit-2.1
 echo ">> Gaviota" && chmod 755 ../../OS/linux/Engines/gaviota/Gaviota-0.84
 echo ">> Glaurung" && chmod 755 ../../OS/linux/Engines/glaurung/Glaurung-2.2
@@ -70,11 +70,11 @@ echo ">> Pigeon" && chmod 755 ../../OS/linux/Engines/pigeon/Pigeon-1.5.1
 echo ">> Pulse" && chmod 755 ../../OS/linux/Engines/pulse/Pulse-1.6.1
 echo ">> Quokka" && chmod 755 ../../OS/linux/Engines/quokka/Quokka-2.1
 echo ">> Rocinante" && chmod 755 ../../OS/linux/Engines/rocinante/Rocinante-2.0
-echo ">> RodentII" && chmod 755 ../../OS/linux/Engines/rodentII/RodentII-0.9.64
+echo ">> RodentII" && chmod 755 ../../OS/linux/Engines/rodentii/RodentII-0.9.64
 echo ">> Shallow-blue" && chmod 755 ../../OS/linux/Engines/shallow-blue/Shallow-blue-2.0.0
 echo ">> Simplex" && chmod 755 ../../OS/linux/Engines/simplex/Simplex-0.9.8
 echo ">> Sissa" && chmod 755 ../../OS/linux/Engines/sissa/Sissa-2.0
-echo ">> SpaceDog" && chmod 755 ../../OS/linux/Engines/spaceDog/SpaceDog-0.97.7
+echo ">> SpaceDog" && chmod 755 ../../OS/linux/Engines/spacedog/SpaceDog-0.97.7
 echo ">> Stash" && chmod 755 ../../OS/linux/Engines/stash/Stash-29.0
 echo ">> Stockfish" && chmod 755 ../../OS/linux/Engines/stockfish/Stockfish-13
 echo ">> Supernova" && chmod 755 ../../OS/linux/Engines/supernova/Supernova-2.3
@@ -85,7 +85,7 @@ echo ">> Tunguska" && chmod 755 ../../OS/linux/Engines/tunguska/Tunguska-1.1
 echo ">> Velvet" && chmod 755 ../../OS/linux/Engines/velvet/Velvet-1.2.0
 echo ">> Weiss" && chmod 755 ../../OS/linux/Engines/weiss/Weiss-1.2
 echo ">> Wowl" && chmod 755 ../../OS/linux/Engines/wowl/Wowl-1.3.7
-echo ">> WyldChess" && chmod 755 ../../OS/linux/Engines/wyldChess/WyldChess-1.51
+echo ">> WyldChess" && chmod 755 ../../OS/linux/Engines/wyldchess/WyldChess-1.51
 echo ">> Zappa" && chmod 755 ../../OS/linux/Engines/zappa/Zappa-1.1
 echo ">> Zurichess" && chmod 755 ../../OS/linux/Engines/zurichess/Zurichess-1.7.4
 echo ""
